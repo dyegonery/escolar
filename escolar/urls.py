937993bd33859e7081499bb2670c0f0408510822
Dyego Nery <dyego@3nw.com.br>
2014-06-11 17:53:50 -0300

    Criacao de rule para redirecionamento padrao para app notas

--- escolar/urls.py
+++ escolar/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = patterns(
     '',
+    url(r'^$', lambda r : HttpResponseRedirect('notas/')),
     url(r'^notas/', include('notas.urls', namespace='notas')),
     url(r'^admin/', include(admin.site.urls)),
 )
